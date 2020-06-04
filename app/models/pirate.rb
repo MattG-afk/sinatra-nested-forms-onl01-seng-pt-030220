5affed8e00edcf72af3db8d73d293580251a7b42
Matt Gordon <bigmattsize21@gmail.com>
2020-06-04 20:09:16 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,17 +1,3 @@
 class Pirate
-  attr_accessor :name, :weight, :height
-  
-  @@all = []
-  
-  def initialize(name,weight,height)
-    @name = params[:name]
-    @weight = params [:weight]
-    @height = params [:height]
-    @@all << self
-  end
-  
-  def self.all
-    @@all
-  end
   
 end
